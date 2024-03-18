86535734e5ed3686efce8274175845962ec298e7
rupambora31 <iamrupam99@gmail.com>
2024-03-18 19:16:16 +0530

    solved findLargestElement

--- assignments/week-1/01-js/easy/findLargestElement.js
+++ assignments/week-1/01-js/easy/findLargestElement.js
@@ -5,6 +5,14 @@
   - Output: 9
 */
 
-function findLargestElement(numbers) {}
+function findLargestElement(numbers) {
+  let biggestElement = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > biggestElement) {
+      biggestElement = numbers[i];
+    }
+  }
+  return biggestElement;
+}
 
 module.exports = findLargestElement;
